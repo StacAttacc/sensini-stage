c5335976a541a3a5faeab6eab507ad49c2e49d8a
Anastasia <cookiedemons@outlook.com>
2023-05-24 11:41:41 -0400

    database link and first model creation

---
+++ Backend/SCSI.Payroll/SCSI.Payroll.DatabaseConception/Model1.edmx.sql
@@ -0,0 +1,56 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 05/24/2023 11:32:25
+-- Generated from EDMX file: B:\Workspace\sensini-stage\Backend\SCSI.Payroll\SCSI.Payroll.DatabaseConception\Model1.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [dbtest];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Employee'
+CREATE TABLE [dbo].[Employee] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [FirstName] nvarchar(250)  NOT NULL,
+    [LastName] nvarchar(250)  NOT NULL,
+    [BirthDate] datetime  NOT NULL,
+    [NAS] nvarchar(9)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Employee'
+ALTER TABLE [dbo].[Employee]
+ADD CONSTRAINT [PK_Employee]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
---
+++ Backend/SCSI.Payroll/SCSI.Payroll.Models/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCSI.Payroll.Models.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public System.DateTime BirthDate { get; set; }
+        public string NAS { get; set; }
+    }
+}
