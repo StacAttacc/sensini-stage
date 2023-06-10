f1d5d47ddca7881b165dac02af140c9746fed34b
Anastasia <cookiedemons@outlook.com>
2023-06-09 23:24:21 -0400

    adding constraints in service

--- Backend/SCSI.Payroll/SCSI.Payroll.Business/Implementations/TaxBracketBusiness.cs
+++ Backend/SCSI.Payroll/SCSI.Payroll.Business/Implementations/TaxBracketBusiness.cs
@@ -12,9 +12,16 @@ namespace SCSI.Payroll.Business.Implementations
     public class TaxBracketBusiness : ITaxBracketBusiness
     {
         private ITaxBracketRepository _taxBracketRepository;
-        public TaxBracketBusiness(ITaxBracketRepository taxBracketRepository)
+        private IFiscalYearBusiness _fiscalYearBusiness;
+        private IGovernmentBusiness _governmentBusiness;
+
+        public TaxBracketBusiness(ITaxBracketRepository taxBracketRepository,
+                                  IFiscalYearBusiness fiscalYearBusiness,
+                                  IGovernmentBusiness governmentBusiness)
         {
             this._taxBracketRepository = taxBracketRepository;
+            this._fiscalYearBusiness = fiscalYearBusiness;
+            this._governmentBusiness = governmentBusiness;
         }
         public async Task<TaxBracket> DeleteTaxBracketByIdAsync(int id)
         {
@@ -57,15 +64,107 @@ namespace SCSI.Payroll.Business.Implementations
 
         public async Task<TaxBracket> SaveTaxBracketAsync(TaxBracket taxBracket)
         {
+            var fiscalYears = await _taxBracketRepository.GetTaxBracketsAsync();
+            var allTaxBrackets = await _taxBracketRepository.GetTaxBracketsAsync();
             try
             {
-                var result = await _taxBracketRepository.SaveTaxBracketAsync(taxBracket);
-                return result;
+                if(ValidateOverlap(_fiscalYearBusiness.GetFiscalYearByIdAsync(taxBracket.FiscalYearId),
+                                    _governmentBusiness.GetGovernmentByIdAsync(taxBracket.GovernmentId),
+                                    taxBracket.LowerLimit,
+                                    taxBracket.UpperLimit,
+                                    fiscalYears))
+                {
+                    if(await ValidateCoverageAsync(taxBracket,
+                                    _fiscalYearBusiness.GetFiscalYearByIdAsync(taxBracket.FiscalYearId),
+                                    _governmentBusiness.GetGovernmentByIdAsync(taxBracket.GovernmentId),
+                                    taxBracket.LowerLimit,
+                                    taxBracket.UpperLimit))
+                    {
+                        var result = await _taxBracketRepository.SaveTaxBracketAsync(taxBracket);
+                        return result;
+                    }
+                }
+                return default;
             }
             catch(Exception ex)
             {
                 throw;
             }
         }
+
+        private bool ValidateOverlap(Task<FiscalYear> fiscalYear, Task<Government> government, decimal lowerLimit, decimal upperLimit, List<TaxBracket> taxBrackets)
+        {
+            if (taxBrackets == null)
+            {
+                return true;
+            }
+            
+            foreach (TaxBracket taxBracket in taxBrackets)
+            {
+                if (taxBracket.FiscalYearId == fiscalYear.Id && taxBracket.GovernmentId == government.Id)
+                {
+                    if (taxBracket.LowerLimit == lowerLimit || taxBracket.UpperLimit == upperLimit)
+                    {
+                        return false;
+                    }
+                    /*if (taxBracket.LowerLimit == upperLimit || taxBracket.UpperLimit == lowerLimit)
+                    {
+                        return false;
+                    }*/
+                    if (taxBracket.LowerLimit < lowerLimit && lowerLimit < taxBracket.UpperLimit)
+                    {
+                        return false;
+                    }
+                    if (taxBracket.UpperLimit > upperLimit && upperLimit > taxBracket.LowerLimit)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private async Task<bool> ValidateCoverageAsync(TaxBracket taxBracketInput ,Task<FiscalYear> fiscalYear, Task<Government> government, decimal lowerLimit, decimal upperLimit)
+        {
+            if (_taxBracketRepository.GetTaxBracketsAsync == null)
+            {
+                return true;
+            }
+            var sortedTaxBracketsList = await SortTaxBracketWithFiscalYearAndGovernmentAsync(fiscalYear, government);
+            var taxBracketToCompare = sortedTaxBracketsList.Last();
+            if(taxBracketToCompare != null)
+            {
+                if(upperLimit != taxBracketToCompare.LowerLimit)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private async Task<List<TaxBracket>> SortTaxBracketWithFiscalYearAndGovernmentAsync(Task<FiscalYear> fiscalYear, Task<Government> government)
+        {
+            var taxBrackets = await _taxBracketRepository.GetTaxBracketsAsync();
+            List<TaxBracket> targetedTaxBrackets = null;
+            List<TaxBracket> sortedTaxBrackets = null;
+            foreach (TaxBracket taxBracket in taxBrackets)
+            {
+                if(taxBracket.FiscalYearId == fiscalYear.Id)
+                {
+                    if(taxBracket.GovernmentId == government.Id)
+                    {
+                        targetedTaxBrackets.Add(taxBracket);
+                    }
+                }
+            }
+            if(targetedTaxBrackets != null)
+            {
+                sortedTaxBrackets = targetedTaxBrackets.OrderBy(tax => tax.LowerLimit).ToList();
+            }
+            return sortedTaxBrackets;
+            
+        }
+
+        
     }
 }
