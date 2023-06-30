92453ef50bc54e3fd0f4baf668941cee6d732592
Anastasia <cookiedemons@outlook.com>
2023-06-29 23:38:25 -0400

    ui error messages

--- Backend/SCSI.Payroll/SCSI.Payroll.WebApi/MiddleWares/ExceptionMiddleware.cs
+++ Backend/SCSI.Payroll/SCSI.Payroll.WebApi/MiddleWares/ExceptionMiddleware.cs
@@ -1,4 +1,8 @@
-﻿namespace SCSI.Payroll.WebApi.MiddleWares
+﻿using SCSI.Payroll.Models.Domains;
+using System.Net;
+using System.Text.Json;
+
+namespace SCSI.Payroll.WebApi.MiddleWares
 {
     public class ExceptionMiddleware : IMiddleware
     {
@@ -17,6 +21,11 @@
         private async Task ManageError(Exception ex, HttpContext context)
         {
             context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            var response = new CustomApiError((int)HttpStatusCode.InternalServerError, ex.Message);
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var json = System.Text.Json.JsonSerializer.Serialize(response, options);
+            await context.Response.WriteAsync(json);
         }
     }
 }
