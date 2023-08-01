e254573ae394455e32be2ea67e9bb016224e9fa7
Anastasia <cookiedemons@outlook.com>
2023-08-01 15:38:52 -0400

    token to backend

--- Frontend/scsi-payroll-ui-web/src/app/services/security-token.interceptor.ts
+++ Frontend/scsi-payroll-ui-web/src/app/services/security-token.interceptor.ts
@@ -3,16 +3,31 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { LoginService } from './payroll-api-proxy';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable()
 export class SecurityTokenInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private authService: LoginService,
+              private cookieService: CookieService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if(request.url.includes('/api/users/login')){
+      return next.handle(request).pipe(
+        tap(event => {
+          if(event instanceof HttpResponse){
+            const decodedToken = event.body;
+            this.cookieService.set('token', decodedToken);
+          }
+        })
+      )
+    }
+
     return next.handle(request);
   }
 }
